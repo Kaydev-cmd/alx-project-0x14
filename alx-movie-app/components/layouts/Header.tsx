a9e70342c48d2990c2dce9e3c6e2ee5c4f4b3b96
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-18 11:38:59 +0200

    feat: create responsive Header component with CineSeek branding

--- alx-movie-app/components/layouts/Header.tsx
+++ alx-movie-app/components/layouts/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header: React.FC = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-28 flex items-center bg-[#171D22] px-4 md:px-16 lg:px-44 text-white">
+      <div className="flex items-center justify-between w-full">
+        <h2 className="text-xl md:text-4xl font-semibold">Cine<span className="text-[#E2D609]">Seek</span></h2>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
