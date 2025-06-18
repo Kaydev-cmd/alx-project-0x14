ffe2f2d65bb5565122a722e24a9b42a9d9a8009e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-18 11:54:00 +0200

    feat: add responsive footer layout with CineSeek branding

--- alx-movie-app/components/layouts/Footer.tsx
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -8,7 +8,16 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-[#171D22] text-white py-10 px-6 md:px-10 lg:px-20">
+      <div className="flex flex-col md:flex-row justify-between items-center w-full">
+        {/* Footer Logo */}
+        <h2 className="text-xl md:text-4xl font-semibold mb-4 md:mb-0">
+          Cine<span className="text-[#E2D609]">Seek</span>
+        </h2>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
