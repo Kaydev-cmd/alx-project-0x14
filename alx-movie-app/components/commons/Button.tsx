513a03d48f06221c5fb6ffea6893a8531f8007cf
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-18 12:46:03 +0200

    feat: add cursor pointer

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -5,7 +5,7 @@ const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
     <button
       onClick={action}
-      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300 cursor-pointer"
     >
       {title}
     </button>
