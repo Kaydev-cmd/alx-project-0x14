2a48c2db998e07fea9b2c7cdf7a4591ac94188d9
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-18 11:49:29 +0200

    feat: import social media icons and Next.js Link for navigation

--- alx-movie-app/components/layouts/Footer.tsx
+++ alx-movie-app/components/layouts/Footer.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer: React.FC = () => {
   return <div>Footer</div>;
