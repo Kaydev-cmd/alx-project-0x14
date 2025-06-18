dc37b13e953bc93960ca876ee79096f9149ba9b6
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-18 12:42:17 +0200

    feat: add Join CineSeek signup and call-to-action section with headline and button

--- alx-movie-app/pages/index.tsx
+++ alx-movie-app/pages/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
         className="h-screen bg-cover bg-center"
         style={{
           backgroundImage:
-            "url('https://themebeyond.com/html/movflx/img/bg/breadcrumb_bg.jpg')",
+            "url('/assets/background.jpg')",
         }}
       >
         <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center text-center">
@@ -28,6 +28,17 @@ const Home = () => {
             action={() => router.push("/movies", undefined, { shallow: false })}
           />
         </div>
+      </section>
+
+      <section className="py-16 px-8 md:px-44 bg-[#121018] text-center">
+        <h2 className="text-3xl md:text-5xl font-semibold mb-8">
+          Join CineSeek Now!
+        </h2>
+        <p className="text-lg md:text-2xl mb-12">
+          Sign up today to get access to the latest movies, exclusive content,
+          and personalized movie recommendations.
+        </p>
+        <Button title="Get Started" />
       </section>
     </div>
   );
