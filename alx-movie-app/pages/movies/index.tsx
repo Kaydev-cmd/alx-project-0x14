8e65df5bf68598310a963a66a88331e5499151e3
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-06-19 08:27:51 +0200

    feat: setup Movies state management with pagination, filters, and loading state

--- alx-movie-app/pages/movies/index.tsx
+++ alx-movie-app/pages/movies/index.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import Button from "@/components/commons/Button";
+import Loading from "@/components/commons/Loading";
+import MovieCard from "@/components/commons/MovieCard";
+import { MovieProps, Mprops } from "@/interfaces";
 
-const Movies: React.FC = () => {
+const Movies: React.FC<Mprops> = () => {
+  const [page, setPage] = useState<number>(1);
+  const [year, setYear] = useState<number | null>(null);
+  const [genre, setGenre] = useState<string>("All");
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
   return <div>Movies</div>;
 };
 
